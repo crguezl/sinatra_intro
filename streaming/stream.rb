efc27c8c311ae8b74eea9d3e17a3781d78f87991
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-11-28 16:32:19 +0000

    m

--- streaming/stream.rb
+++ streaming/stream.rb
@@ -6,8 +6,18 @@ end
 
 class Flujo
   def each
-    100.times { |i| yield "#{i}\n"}
+    100.times do |i| 
+      yield "#{i}\n"
+:qa
+    end
   end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    body = body.each.map { |s| s.upcase }
+    [status, headers, body]
+  end
+
 end
 
 get '/' do
